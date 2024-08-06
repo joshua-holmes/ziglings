ab70bc6efd0801e872d1372ca27ed02ad1b18436
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2024-08-05 21:56:49 -0700

    Solve problem 51

--- exercises/051_values.zig
+++ exercises/051_values.zig
@@ -87,7 +87,7 @@ pub fn main() void {
     // Let's assign the std.debug.print function to a const named
     // "print" so that we can use this new name later!
 
-    const print = ???;
+    const print = std.debug.print;
 
     // Now let's look at assigning and pointing to values in Zig.
     //
@@ -130,7 +130,7 @@ pub fn main() void {
     // the address it points to is still mutable! So we CAN change it.
 
     const glorp_access3: *Character = &glorp;
-    glorp_access3.gold = 333;
+    // glorp_access3.gold = 333;
     print("3:{}!. ", .{glorp.gold == glorp_access3.gold});
 
     // NOTE:
@@ -163,13 +163,13 @@ pub fn main() void {
     print("XP before:{}, ", .{glorp.experience});
 
     // Fix 1 of 2 goes here:
-    levelUp(glorp, reward_xp);
+    levelUp(glorp_access3, reward_xp);
 
     print("after:{}.\n", .{glorp.experience});
 }
 
 // Fix 2 of 2 goes here:
-fn levelUp(character_access: Character, xp: u32) void {
+fn levelUp(character_access: *Character, xp: u32) void {
     character_access.experience += xp;
 }
 
